44ee201f635e586fd6ed21629af05e3cf8d55497
wildcard98 <mumojames98@gmail.com>
2023-05-30 14:50:10 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,9 +25,9 @@ const Navbar = () => {
         <li>
           <a href="#skills">Skills</a>
         </li>
-        {/* <li>
+        <li>
           <a href="#experience">Experience</a>
-        </li> */}
+        </li>
         <li>
           <a href="#projects">Projects</a>
         </li>
